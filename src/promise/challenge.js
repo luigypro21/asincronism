5cfb33ed3c69d9b7322de64329dc4da1a5caa14e
luigy <luigy@DESKTOP-FDGMJJA.local>
2020-07-18 15:41:55 -0500

    final commit

---
+++ src/promise/challenge.js
@@ -0,0 +1,17 @@
+const fecthData = require("../utils/fetchData");
+const fetchData = require("../utils/fetchData");
+const API = "https://rickandmortyapi.com/api/character/";
+
+fecthData(API)
+  .then((data) => {
+    console.log(data.info.count);
+    return fetchData(`${API}${data.results[0].id}`);
+  })
+  .then((data) => {
+    console.log(data.name);
+    return fecthData(data.origin.url);
+  })
+  .then((data) => {
+    console.log(data.dimension);
+  })
+  .catch((err) => console.error(err));
---
+++ src/promise/index.js
@@ -0,0 +1,39 @@
+const somethinWillHappen = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("Hey!");
+    } else {
+      reject("Whoops!");
+    }
+  });
+};
+
+somethinWillHappen()
+  .then((response) => console.log(response))
+  .catch((error) => console.error(error));
+
+const somethingWillHappen2 = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      setTimeout(() => {
+        resolve("True");
+      }, 2000);
+    } else {
+      const error = new Error("Whooops!");
+      reject(error);
+    }
+  });
+};
+
+somethingWillHappen2()
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+
+Promise.all([somethinWillHappen(),somethingWillHappen2()])
+.then(response =>{
+    console.log("array of results",response);
+})
+.catch(err=>{
+    console.error(err);
+})
